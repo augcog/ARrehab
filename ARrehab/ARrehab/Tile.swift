ded542c1b217a78ab4c74fe662b84fc4cf6728b0
Eric Wang <erwang@berkeley.edu>
2020-02-24 23:44:54 -0800

    made tile collision box bigger

--- ARrehab/ARrehab/Tile.swift
+++ ARrehab/ARrehab/Tile.swift
@@ -18,7 +18,7 @@ class Tile : Entity, HasModel, HasCollision {
         self.tileName = name
         super.init()
         self.components[ModelComponent] = ModelComponent(mesh: MeshResource.generateBox(width: 0.5, height: 0.01, depth: 0.5, cornerRadius: 0.2), materials: [SimpleMaterial()])
-        self.components[CollisionComponent] = CollisionComponent(shapes: [ShapeResource.generateBox(width: 0.5, height: 2.0, depth: 0.5)], mode: .trigger, filter: .sensor)
+        self.components[CollisionComponent] = CollisionComponent(shapes: [ShapeResource.generateBox(width: 0.5, height: 4.0, depth: 0.5)], mode: .trigger, filter: .sensor)
         self.transform.translation = SIMD3<Float>(x,0.0,z)
         print("Generated Tile: " + name)
         
